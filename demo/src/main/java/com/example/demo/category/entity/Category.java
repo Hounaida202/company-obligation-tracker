064507f4133c11a9b7d8e94d041554a69326778d
Hounaida202 <etrichih@gmail.com>
2026-03-17 20:34:34 +0000

    refactor

--- demo/src/main/java/com/example/demo/category/entity/Category.java
+++ demo/src/main/java/com/example/demo/category/entity/Category.java
@@ -1,4 +1,22 @@
 package com.example.demo.category.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(length = 500)
+    private String description;
 }
