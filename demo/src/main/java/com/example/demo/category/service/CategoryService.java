ca9339429055babfe7550fbc22f07835015ba9d4
Hounaida202 <etrichih@gmail.com>
2026-03-17 20:26:55 +0000

    feat: creation de la methode service de recuperation de toutes le categories

---
+++ demo/src/main/java/com/example/demo/category/service/CategoryService.java
@@ -0,0 +1,18 @@
+package com.example.demo.category.service;
+
+import com.example.demo.category.entity.Category;
+import com.example.demo.category.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
