7f06ac0dbda2b56d6d86e6dc2264c847fa922477
Hounaida202 <etrichih@gmail.com>
2026-03-22 02:33:38 +0000

    feat: creation de rest controller obligation et l'endpoint obligations

---
+++ demo/src/main/java/com/example/demo/obligation/controller/ObligationController.java
@@ -0,0 +1,22 @@
+package com.example.demo.obligation.controller;
+
+import com.example.demo.obligation.dto.ObligationDto;
+import com.example.demo.obligation.service.ObligationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/obligations")
+public class ObligationController {
+
+    @Autowired
+    private ObligationService obligationService;
+
+    @GetMapping
+    public List<ObligationDto> getAllObligations() {
+        return obligationService.getAllObligations();
+    }
+}
