ff2f418b076615c522f4705267a09c17cef3747d
Hounaida202 <etrichih@gmail.com>
2026-03-23 21:50:40 +0000

    feat: creation de la classe dto pour l envoie des donnés de login a la base

--- demo/src/main/java/com/example/demo/user/dto/LoginRequest.java
+++ demo/src/main/java/com/example/demo/user/dto/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.example.demo.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
+    private String username;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
 }
