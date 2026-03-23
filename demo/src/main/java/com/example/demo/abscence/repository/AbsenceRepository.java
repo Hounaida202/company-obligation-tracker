62893dce9a0cd05949dcb5656b1fce618f6c59f0
Hounaida202 <etrichih@gmail.com>
2026-03-23 03:40:31 +0000

    feat: creation de repository de la bsence

--- demo/src/main/java/com/example/demo/abscence/repository/AbsenceRepository.java
+++ demo/src/main/java/com/example/demo/abscence/repository/AbsenceRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.abscence.repository;
 
-public interface AbsenceRepository {
+import com.example.demo.abscence.entity.Absence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AbsenceRepository extends JpaRepository<Absence, Long> {
+    List<Absence> findByEmployeeId(Long employeeId);
 }
