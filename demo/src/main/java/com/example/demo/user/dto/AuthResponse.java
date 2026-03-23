ee269bda1b65fe54513e0035124f7bf295c54109
Hounaida202 <etrichih@gmail.com>
2026-03-23 21:50:06 +0000

    feat: creation de dto entité pour l'envoie de la reponse d authentification

--- demo/src/main/java/com/example/demo/user/dto/AuthResponse.java
+++ demo/src/main/java/com/example/demo/user/dto/AuthResponse.java
@@ -1,4 +1,13 @@
 package com.example.demo.user.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
+    private String token;
+    private UserDto user;
 }
