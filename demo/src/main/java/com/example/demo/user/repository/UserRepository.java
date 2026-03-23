c7022872c7da2f48b1ea4bc1c575d34151f77f2a
Hounaida202 <etrichih@gmail.com>
2026-03-23 03:28:17 +0000

    feat: creation de repo user pour interagir avec la bd

--- demo/src/main/java/com/example/demo/user/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.user.repository;
 
-public interface UserRepository {
+import com.example.demo.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
