77d82681af96b668f16b3a8256b2585863696fc3
Hounaida202 <etrichih@gmail.com>
2026-03-23 03:40:03 +0000

    feat: creation de interface mapper pour convertion entre dto et absence

--- demo/src/main/java/com/example/demo/abscence/mapper/AbsenceMapper.java
+++ demo/src/main/java/com/example/demo/abscence/mapper/AbsenceMapper.java
@@ -1,4 +1,12 @@
 package com.example.demo.abscence.mapper;
 
-public class AbsenceMapper {
+import com.example.demo.abscence.dto.AbsenceDto;
+import com.example.demo.abscence.entity.Absence;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AbsenceMapper {
+    AbsenceDto toDto(Absence absence);
+
+    Absence toEntity(AbsenceDto dto);
 }
