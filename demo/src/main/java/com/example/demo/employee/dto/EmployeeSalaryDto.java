642d46958186e0171b48eedf3127cb6835480524
Hounaida202 <etrichih@gmail.com>
2026-03-24 10:38:04 +0000

    feat: l ajout de l entité dto employéé salaire pour le retour des salires

--- demo/src/main/java/com/example/demo/employee/dto/EmployeeSalaryDto.java
+++ demo/src/main/java/com/example/demo/employee/dto/EmployeeSalaryDto.java
@@ -1,4 +1,15 @@
 package com.example.demo.employee.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EmployeeSalaryDto {
+    private Double baseSalary;
+    private Double totalDeduction;
+    private Double netSalary;
+    private Integer absencesCountThisMonth;
 }
