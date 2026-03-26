f034bf10a5c000edda8503ecdc201860c302e179
Hounaida202 <etrichih@gmail.com>
2026-03-26 09:05:12 +0000

    feat : creation de controller job categori avec l endmpoit get job categories

--- demo/src/main/java/com/example/demo/jobCategory/controller/JobCategoryController.java
+++ demo/src/main/java/com/example/demo/jobCategory/controller/JobCategoryController.java
@@ -1,4 +1,21 @@
 package com.example.demo.jobCategory.controller;
 
+import com.example.demo.jobCategory.entity.JobCategory;
+import com.example.demo.jobCategory.service.JobCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobCategories")
 public class JobCategoryController {
+
+    @Autowired
+    private JobCategoryService jobCategoryService;
+
+    @GetMapping
+    public List<JobCategory> getAllJobCategories() {
+        return jobCategoryService.getAllJobCategories();
+    }
 }
