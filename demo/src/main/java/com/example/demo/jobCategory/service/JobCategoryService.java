b19308e5759a9dba7490bffd9320e3c7da032938
Hounaida202 <etrichih@gmail.com>
2026-03-26 09:03:58 +0000

    feat : creation de la classe service de job category avec la methode de get job categories

--- demo/src/main/java/com/example/demo/jobCategory/service/JobCategoryService.java
+++ demo/src/main/java/com/example/demo/jobCategory/service/JobCategoryService.java
@@ -1,4 +1,20 @@
 package com.example.demo.jobCategory.service;
 
+
+import com.example.demo.jobCategory.entity.JobCategory;
+import com.example.demo.jobCategory.repository.JobCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class JobCategoryService {
+
+    @Autowired
+    private JobCategoryRepository jobCategoryRepository;
+
+    public List<JobCategory> getAllJobCategories() {
+        return jobCategoryRepository.findAll();
+    }
 }
