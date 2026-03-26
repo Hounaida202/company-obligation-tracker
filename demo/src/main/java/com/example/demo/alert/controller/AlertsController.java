e61825d1749a7f19d729935e0288e8f7edfa17a7
Hounaida202 <etrichih@gmail.com>
2026-03-26 23:30:56 +0000

    feat: restreindre l'accès à la méthode aux utilisateurs ADMIN avec @PreAuthorize

--- demo/src/main/java/com/example/demo/alert/controller/AlertsController.java
+++ demo/src/main/java/com/example/demo/alert/controller/AlertsController.java
@@ -3,6 +3,7 @@ package com.example.demo.alert.controller;
 import com.example.demo.alert.dto.AlertsDto;
 import com.example.demo.alert.service.AlertsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ public class AlertsController {
     private AlertsService alertsService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+
     public AlertsDto getAlerts() {
         return alertsService.getAlerts();
     }
