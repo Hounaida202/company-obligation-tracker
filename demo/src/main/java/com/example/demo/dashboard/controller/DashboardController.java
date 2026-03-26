3bd7df771ff8205f1ddab131e8abaf2a2c31076c
Hounaida202 <etrichih@gmail.com>
2026-03-26 23:13:09 +0000

    feat: creation de l endpoint stats

--- demo/src/main/java/com/example/demo/dashboard/controller/DashboardController.java
+++ demo/src/main/java/com/example/demo/dashboard/controller/DashboardController.java
@@ -1,4 +1,22 @@
 package com.example.demo.dashboard.controller;
 
+
+import com.example.demo.dashboard.dto.DashboardStatsDto;
+import com.example.demo.dashboard.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
 public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/stats")
+    public DashboardStatsDto getStats() {
+        return dashboardService.getStats();
+    }
 }
