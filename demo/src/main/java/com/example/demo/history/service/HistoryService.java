0968177b15af92dac36fa9630fd227961f608db5
Hounaida202 <etrichih@gmail.com>
2026-03-26 10:15:05 +0000

    feat : la creation de la methode service de la recuperation des historiques

--- demo/src/main/java/com/example/demo/history/service/HistoryService.java
+++ demo/src/main/java/com/example/demo/history/service/HistoryService.java
@@ -1,4 +1,29 @@
 package com.example.demo.history.service;
 
+import com.example.demo.history.dto.HistoryDto;
+import com.example.demo.history.entity.History;
+import com.example.demo.history.mapper.HistoryMapper;
+import com.example.demo.history.repository.HistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class HistoryService {
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    private HistoryMapper historyMapper;
+
+    public List<HistoryDto> getAllHistory() {
+        return historyRepository.findAll().stream()
+                .map(historyMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+
 }
