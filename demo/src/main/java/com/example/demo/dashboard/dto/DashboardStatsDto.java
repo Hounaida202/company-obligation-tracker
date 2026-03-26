63737b2a3726671b1c204ec33dd7702660018983
Hounaida202 <etrichih@gmail.com>
2026-03-26 09:02:11 +0000

    feat: creation de l entity dto pour les stats du dashboard

--- demo/src/main/java/com/example/demo/dashboard/dto/DashboardStatsDto.java
+++ demo/src/main/java/com/example/demo/dashboard/dto/DashboardStatsDto.java
@@ -1,4 +1,25 @@
 package com.example.demo.dashboard.dto;
 
+import com.example.demo.obligation.dto.ObligationDto;
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DashboardStatsDto {
+    private Long totalEmployees;
+    private Long activeObligations;
+    private Double totalMonthlyObligations;
+    private Integer upcomingAlerts;
+
+    private Double totalAmountThisMonth;
+    private Double paidAmountThisMonth;
+    private Long obligationsCountThisMonth;
+
+    private List<ObligationDto> todayPending;
+    private List<ObligationDto> todayPaid;
+    private List<ObligationDto> monthTable;
 }
