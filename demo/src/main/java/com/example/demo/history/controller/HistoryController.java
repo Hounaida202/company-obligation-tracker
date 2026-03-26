8aea356c65276822a0d50ce1d7974b4139190afa
Hounaida202 <etrichih@gmail.com>
2026-03-26 10:16:25 +0000

    feat ; creation de l endpoint dans controller des historiques pour la recuperation

--- demo/src/main/java/com/example/demo/history/controller/HistoryController.java
+++ demo/src/main/java/com/example/demo/history/controller/HistoryController.java
@@ -1,4 +1,22 @@
 package com.example.demo.history.controller;
 
+import com.example.demo.history.dto.HistoryDto;
+import com.example.demo.history.service.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/history")
 public class HistoryController {
+
+    @Autowired
+    private HistoryService historyService;
+
+    @GetMapping
+    public ResponseEntity<List<HistoryDto>> getAllHistory() {
+        return ResponseEntity.ok(historyService.getAllHistory());
+    }
 }
