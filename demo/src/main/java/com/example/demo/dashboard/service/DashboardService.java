6c948f62239a8056b1d20b80134cfe673843a447
Hounaida202 <etrichih@gmail.com>
2026-03-26 10:31:26 +0000

    feat : creation de classe service pour les statistiques , et la squelette de la fonction get stats avec l'injection des dependances necessaires dans la methode

--- demo/src/main/java/com/example/demo/dashboard/service/DashboardService.java
+++ demo/src/main/java/com/example/demo/dashboard/service/DashboardService.java
@@ -1,4 +1,46 @@
 package com.example.demo.dashboard.service;
 
+import com.example.demo.dashboard.dto.DashboardStatsDto;
+import com.example.demo.employee.dto.EmployeeSalaryDto;
+import com.example.demo.employee.repository.EmployeeRepository;
+import com.example.demo.employee.service.EmployeeService;
+import com.example.demo.history.entity.History;
+import com.example.demo.history.repository.HistoryRepository;
+import com.example.demo.jobCategory.entity.JobCategory;
+import com.example.demo.jobCategory.repository.JobCategoryRepository;
+import com.example.demo.obligation.dto.ObligationDto;
+import com.example.demo.obligation.entity.Obligation;
+import com.example.demo.obligation.mapper.ObligationMapper;
+import com.example.demo.obligation.repository.ObligationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DashboardService {
-}
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObligationRepository obligationRepository;
+
+    @Autowired
+    private ObligationMapper obligationMapper;
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    private JobCategoryRepository jobCategoryRepository;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public DashboardStatsDto getStats() {
+
+     }
