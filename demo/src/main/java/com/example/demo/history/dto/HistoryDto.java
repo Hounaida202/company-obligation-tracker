b4416e7470be747a6224da83612a3df914190af1
Hounaida202 <etrichih@gmail.com>
2026-03-26 08:55:57 +0000

    feat: creation de l 'entity dto de history

--- demo/src/main/java/com/example/demo/history/dto/HistoryDto.java
+++ demo/src/main/java/com/example/demo/history/dto/HistoryDto.java
@@ -1,4 +1,15 @@
 package com.example.demo.history.dto;
 
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class HistoryDto {
+    private Long id;
+    private String type;
+    private Long referenceId;
+    private String title;
+    private Double amount;
+    private String currency;
+    private LocalDate paymentDate;
 }
