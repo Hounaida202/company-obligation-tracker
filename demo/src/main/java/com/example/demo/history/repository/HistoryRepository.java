2882edadeb5be83702f769c46233d4baef0f8d76
Hounaida202 <etrichih@gmail.com>
2026-03-26 08:56:27 +0000

    feat; creation de l interface repo de history

--- demo/src/main/java/com/example/demo/history/repository/HistoryRepository.java
+++ demo/src/main/java/com/example/demo/history/repository/HistoryRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.history.repository;
 
-public class HistoryRepository {
+import com.example.demo.history.entity.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long> {
+    List<History> findByTypeAndReferenceId(String type, Long referenceId);
+    List<History> findByPaymentDateBetween(LocalDate start, LocalDate end);
+    List<History> findByPaymentDate(LocalDate date);
 }
