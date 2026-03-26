20eb5b67ef08fbd151ac7ca5f92a867c57537b73
Hounaida202 <etrichih@gmail.com>
2026-03-26 08:57:51 +0000

    feat : creation de l interface mapper pour la convertion entity dto

--- demo/src/main/java/com/example/demo/history/mapper/HistoryMapper.java
+++ demo/src/main/java/com/example/demo/history/mapper/HistoryMapper.java
@@ -1,4 +1,34 @@
 package com.example.demo.history.mapper;
 
+import com.example.demo.history.dto.HistoryDto;
+import com.example.demo.history.entity.History;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HistoryMapper {
+    public HistoryDto toDto(History entity) {
+        if (entity == null) return null;
+        return HistoryDto.builder()
+                .id(entity.getId())
+                .type(entity.getType())
+                .referenceId(entity.getReferenceId())
+                .title(entity.getTitle())
+                .amount(entity.getAmount())
+                .currency(entity.getCurrency())
+                .paymentDate(entity.getPaymentDate())
+                .build();
+    }
+
+    public History toEntity(HistoryDto dto) {
+        if (dto == null) return null;
+        return History.builder()
+                .id(dto.getId())
+                .type(dto.getType())
+                .referenceId(dto.getReferenceId())
+                .title(dto.getTitle())
+                .amount(dto.getAmount())
+                .currency(dto.getCurrency())
+                .paymentDate(dto.getPaymentDate())
+                .build();
+    }
 }
