ca17fc42ccaa6c1feb55e23a3d85a9cd3f2f269d
Hounaida202 <etrichih@gmail.com>
2026-03-26 23:23:50 +0000

    feat : creation du controller avec l endpoint getalerts

--- demo/src/main/java/com/example/demo/alert/controller/AlertsController.java
+++ demo/src/main/java/com/example/demo/alert/controller/AlertsController.java
@@ -1,4 +1,21 @@
 package com.example.demo.alert.controller;
 
+import com.example.demo.alert.dto.AlertsDto;
+import com.example.demo.alert.service.AlertsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alerts")
 public class AlertsController {
+
+    @Autowired
+    private AlertsService alertsService;
+
+    @GetMapping
+    public AlertsDto getAlerts() {
+        return alertsService.getAlerts();
+    }
 }
